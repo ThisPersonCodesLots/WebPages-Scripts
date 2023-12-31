8a03482447ae16ed36d66e3caf13bc5f92ebb6d2
borntorave <viorelm41@gmail.com>
2023-12-31 09:18:42 +0200

    Add video saturation control

--- HTML5_manipulation/main.js
+++ HTML5_manipulation/main.js
@@ -9,8 +9,12 @@
 // ==/UserScript==
 
 let video;
+// Speed of the video
 let videoSpeed = 1;
 let speedDisplay;
+// Saturation of the video
+let videoSaturation = 1;
+let videoSaturationDisplay;
 
 document.addEventListener("playing", registerShortcutKeys, { capture: true, once: true });
 document.addEventListener("playing", restoreSpeed, { capture: true });
@@ -33,6 +37,23 @@ function createSpeedDisplay(video) {
   setTimeout(() => speedDisplay.style.opacity = '0', 5000);
 }
 
+function createSaturationDisplay(video) {
+  videoSaturationDisplay = document.createElement('div');
+  videoSaturationDisplay.textContent = `Saturation: ${videoSaturation.toFixed(2)}`;
+  Object.assign(videoSaturationDisplay.style, {
+    position: 'fixed',
+    top: '50px',
+    left: '10px',
+    backgroundColor: 'black',
+    color: 'lightcoral',
+    padding: '5px',
+    zIndex: '2147483647',
+    opacity: '0'
+  });
+  video.parentNode.appendChild(videoSaturationDisplay);
+  setTimeout(() => videoSaturationDisplay.style.opacity = '0', 5000);
+}
+
 function registerShortcutKeys(event) {
   captureActiveVideoElement(event);
   document.addEventListener("keydown", handlePressedKey);
@@ -40,14 +61,6 @@ function registerShortcutKeys(event) {
 
 function restoreSpeed(event) {
   if (event.target.playbackRate !== videoSpeed) event.target.playbackRate = videoSpeed;
-}
-
-function captureActiveVideoElement(event) {
-  if (event.target.tagName.toLowerCase() === 'video') {
-    video = event.target;
-    videoSpeed = video.playbackRate;
-    createSpeedDisplay(video);
-  }
 }
 
 function handlePressedKey(event) {
@@ -62,6 +75,10 @@ function handlePressedKey(event) {
   else if (key === "]") newRate = Math.min(5, video.playbackRate + 0.05);
   // reset video speed to 1
   else if (key === "r") newRate = 1;
+  // increase saturation by 10%
+  else if (key === "+") videoSaturation = Math.min(4, videoSaturation + 0.1);
+  // decrease saturation by 10%
+  else if (key === "-") videoSaturation = Math.max(0, videoSaturation - 0.1);
 
   if (newRate !== video.playbackRate) {
     video.playbackRate = newRate;
@@ -69,5 +86,23 @@ function handlePressedKey(event) {
     speedDisplay.textContent = `Speed: ${videoSpeed.toFixed(2)}`;
     speedDisplay.style.opacity = '1';
     setTimeout(() => speedDisplay.style.opacity = '0', 10000);
+  }
+
+  // Apply saturation filter
+  if (videoSaturation !== 1) {
+    video.style.filter = `saturate(${videoSaturation * 100}%)`;
+    videoSaturationDisplay.textContent = `Saturation: ${(videoSaturation * 100).toFixed(0)}%`;
+    videoSaturationDisplay.style.opacity = '1';
+    setTimeout(() => videoSaturationDisplay.style.opacity = '0', 5000);
+  }
+}
+
+function captureActiveVideoElement(event) {
+  if (event.target.tagName.toLowerCase() === 'video') {
+    video = event.target;
+    videoSpeed = video.playbackRate;
+    videoSaturation = 1;
+    createSpeedDisplay(video);
+    createSaturationDisplay(video);
   }
 }
